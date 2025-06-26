447d3faae9b995f3c3826884e18615b2badd110a
Sinbad the Coder <sinbadthecoder@proton.me>
2025-06-26 06:55:16 -0500

    create struct with literal value assignment

---
+++ structs/structs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your (MM/DD/YYYY): ")
+
+	var appUser user
+	appUser = user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	outputUserDetails(appUser)
+}
+
+func outputUserDetails(userData user) {
+	fmt.Println(userData)
+}
+
+func getUserData(promptText string) string {
+	fmt.Println(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
