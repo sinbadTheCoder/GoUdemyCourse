c4f704f6ed9bd6ce1b6f9c22094cc8c258853174
Sinbad the Coder <sinbadthecoder@proton.me>
2025-06-30 20:13:12 -0500

    add todo, a permutation of note that is outputable like a note

---
+++ notes/todo/todo.go
@@ -0,0 +1,39 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Todo struct {
+	Text string `json:"text"`
+}
+
+func (todo Todo) Display() {
+	fmt.Println("\n------------------------")
+	fmt.Println("| Todo:", todo.Text)
+	fmt.Println("------------------------")
+}
+
+func (todo Todo) Save() error {
+	fileName := "todo.json"
+
+	json, err := json.Marshal(todo)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, json, 0644)
+}
+
+func New(content string) (Todo, error) {
+	if content == "" {
+		return Todo{}, errors.New("content cannot be empty")
+	}
+
+	return Todo{
+		Text: content,
+	}, nil
+}
