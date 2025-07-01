1623eff05ee119e86bdf9eaf4a50109654725bc7
Sinbad the Coder <sinbadthecoder@proton.me>
2025-06-30 20:11:20 -0500

    use interface to abstract save and display methods

--- notes/main.go
+++ notes/main.go
@@ -8,7 +8,44 @@ import (
 	"strings"
 
 	"example.com/notes/note"
+	"example.com/notes/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+type displayer interface {
+	Display()
+}
+
+type outputable interface {
+	saver
+	displayer
+}
+
+func outputData(data outputable) error {
+	err := saveData(data)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	displayData(data)
+	return nil
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func displayData(data displayer) {
+	data.Display()
+}
 
 func main() {
 	theNote, err := getNoteData()
@@ -17,14 +54,29 @@ func main() {
 		return
 	}
 
-	theNote.Display()
-	err = theNote.Save()
+	theTodo, err := getTodo()
 	if err != nil {
-		fmt.Println("Save failed")
+		fmt.Println(err)
 		return
 	}
 
+	outputData(theTodo)
+	outputData(theNote)
+
 	fmt.Println("Save succeeded")
+}
+
+func getTodo() (t todo.Todo, err error) {
+	fmt.Println("Enter new todo below...")
+	content, _ := getUserInput("Todo content:")
+
+	t, err = todo.New(content)
+	if err != nil {
+		fmt.Println(err)
+		return todo.Todo{}, err
+	}
+
+	return t, nil
 }
 
 func getNoteData() (n note.Note, err error) {
