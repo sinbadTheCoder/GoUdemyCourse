0466af31d7ef468f73e2b66991984e615ddc2cbb
Sinbad the Coder <sinbadthecoder@proton.me>
2025-08-07 10:57:20 -0500

    added make map example and added alt instantiations to compare

--- arrays/main.go
+++ arrays/main.go
@@ -21,13 +21,57 @@ func main() {
 	//
 	// Make
 	//
-	userNames := make([]string, 2, 5)
 
-	userNames[0] = "Moses"
-	userNames[1] = "Joshua"
+	makeSlice()
+	makeMap()
+}
 
-	userNames = append(userNames, "John")
-	userNames = append(userNames, "Mark")
+func makeSlice() {
+	//create array
+	namesArray := [4]string{}
 
-	fmt.Println(userNames)
+	namesArray[0] = "Moses"
+	namesArray[1] = "Joshua"
+	namesArray[2] = "John"
+	namesArray[3] = "Mark"
+
+	fmt.Println("Usernames array:", namesArray)
+
+	//create slice
+	namesSlice := []string{"Moses", "Joshua"}
+
+	namesSlice = append(namesSlice, "John")
+	namesSlice = append(namesSlice, "Mark")
+
+	fmt.Println("Usernames array:", namesSlice)
+
+	//create a slice through make
+	namesSlice = make([]string, 2, 5)
+
+	namesSlice[0] = "Moses"
+	namesSlice[1] = "Joshua"
+	namesSlice = append(namesSlice, "John")
+	namesSlice = append(namesSlice, "Mark")
+
+	fmt.Println("Usernames array through Make slice", namesSlice)
+}
+
+func makeMap() {
+	languagePopularityMap := map[string]float64{}
+	languagePopularityMap["php"] = 4.1
+	languagePopularityMap["go"] = 4.3
+	languagePopularityMap["java"] = 4.5
+	languagePopularityMap["c"] = 3.2
+	languagePopularityMap["rust"] = 3.9
+
+	fmt.Println("Language Popularities:", languagePopularityMap)
+
+	languagePopularityMap = make(map[string]float64, 5)
+	languagePopularityMap["php"] = 4.1
+	languagePopularityMap["go"] = 4.3
+	languagePopularityMap["java"] = 4.5
+	languagePopularityMap["c"] = 3.2
+	languagePopularityMap["rust"] = 3.9
+
+	fmt.Println("Language Popularities (from make):", languagePopularityMap)
 }
